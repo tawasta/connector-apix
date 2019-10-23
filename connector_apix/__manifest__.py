34e85206c78b4b0c8cab0b128321390ab5743cd9
Jarmo Kortetjärvi <jarmo.kortetjarvi@tawasta.fi>
2019-10-23 10:48:29 +0300

    [FIX] Update finvoice string fetching to use getter method

--- connector_apix/__manifest__.py
+++ connector_apix/__manifest__.py
@@ -22,7 +22,7 @@
 {
     'name': 'APIX Connector',
     'summary': 'APIX EDI connector for receiving and sending eInvoices',
-    'version': '10.0.0.11.10',
+    'version': '10.0.0.11.11',
     'category': 'Connector',
     'website': 'http://www.tawasta.fi',
     'author': 'Oy Tawasta Technologies Ltd.',
