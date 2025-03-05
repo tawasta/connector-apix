a14d29fdab27cf325de74d3164b36d34db25007a
Jarmo Kortetjärvi <jarmo.kortetjarvi@tawasta.fi>
2025-03-05 15:42:40 +0200

    [IMP] Force invoice company

--- connector_apix/models/apix_backend.py
+++ connector_apix/models/apix_backend.py
@@ -551,7 +551,12 @@ class ApixBackend(models.Model):
                 # The actual invoice data
                 invoice = self.env["account.move"]._import_finvoice(
                     ET.fromstring(file_data),
-                    self.env["account.move"].create({"move_type": "in_invoice"}),
+                    self.env["account.move"].create(
+                        {
+                            "move_type": "in_invoice",
+                            "company_id": company_id,
+                        }
+                    ),
                     company_id,
                 )
             else:
